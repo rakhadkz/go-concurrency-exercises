490ee0cad23866424e475485fcefabd6993bfe58
Shyngys Rakhad <dev.chinga@gmail.com>
2021-04-23 18:26:59 +0600

    fix: 2 tasks

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,18 +12,21 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, ticker <-chan time.Time) {
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
-
+	<-ticker
+	
 	body, urls, err := fetcher.Fetch(url)
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -35,15 +38,16 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, ticker)
 	}
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
+	ticker := time.Tick(time.Second)
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, ticker)
 	wg.Wait()
 }
